0840b66492ee5c9ed7635400c99dc91e1279f5ba
Con Kolivas <kernel@kolivas.org>
2020-05-23 12:31:45 +1000

    No flush for performance.

--- src/flatfile.cpp
+++ src/flatfile.cpp
@@ -80,6 +80,7 @@ size_t FlatFileSeq::Allocate(const FlatFilePos& pos, size_t add_size, bool& out_
 
 bool FlatFileSeq::Flush(const FlatFilePos& pos, bool finalize)
 {
+return true;
     FILE* file = Open(FlatFilePos(pos.nFile, 0)); // Avoid fseek to nPos
     if (!file) {
         return error("%s: failed to open file %d", __func__, pos.nFile);
